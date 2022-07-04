0f3207880d11ee6d8568aad545b55ef919a5ec3b
Thien.VuXuan <vuxuanthien2001@gmail.com>
2022-07-04 21:22:26 +0700

    'thien'

---
+++ bin/dart_application_3.dart
@@ -0,0 +1,5 @@
+import 'package:dart_application_3/dart_application_3.dart' as dart_application_3;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application_3.calculate()}!');
+}
---
+++ lib/dart_application_3.dart
@@ -0,0 +1,6 @@
+int calculate() {
+  return 6 * 7;
+}
+void main(List<String> args) {
+  
+}
